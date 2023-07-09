25e4b3d12bbf7f2688cab76461b982f12a320e90
DayTroy <shadowyguy5@gmail.com>
2023-07-09 16:52:57 +0300

    ReactMarkdown connected

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
+import ReactMarkdown from 'react-markdown';
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [markdown, setMarkDown] = useState('#Markdown Preview')
+  const handleChange = event => setMarkDown(event.target.value)
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <section className="markdown">
+        <textarea id='editor' value={markdown} onChange={handleChange}></textarea>
+        <article id='preview'>
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
+      </section>
+    </main>
+  );
 }
 
 export default App
